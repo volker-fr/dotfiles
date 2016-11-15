2673625dbd44f0e21f65437fce5aef1c5a211c8c
Volker <lists.volker@gmail.com>
2016-11-15 12:04:55 -0500

    Make installer be able to handle multiple OS

--- install.sh
+++ install.sh
@@ -1,30 +1,66 @@
-sudo apt install -y encfs
-sudo apt install -y iotop vim git redshift-gtk tmux keepass2
-sudo apt install -y owncloud-client
+#!/bin/sh -e -x
+ubuntu() {
+    sudo apt install -y encfs
+    sudo apt install -y iotop vim git redshift-gtk tmux keepass2
+    sudo apt install -y owncloud-client
 
-# i3
-echo "deb http://debian.sur5r.net/i3/ $(lsb_release -c -s) universe" |sudo tee -a /etc/apt/sources.list.d/i3wm.list
-sudo apt update
-sudo apt --allow-unauthenticated install -y sur5r-keyring
-sudo apt install -y i3
+    # i3
+    echo "deb http://debian.sur5r.net/i3/ $(lsb_release -c -s) universe" |sudo tee /etc/apt/sources.list.d/i3wm.list
+    sudo apt update
+    sudo apt --allow-unauthenticated install -y sur5r-keyring
+    sudo apt install -y i3
 
-# virtualbox
-echo "deb http://download.virtualbox.org/virtualbox/debian $(lsb_release -cs) contrib non-free" |sudo tee /etc/apt/sources.list.d/virtualbox.list
-wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
-sudo apt update
-sudo apt install -y virtualbox virtualbox-guest-additions-iso
+    # virtualbox
+    echo "deb http://download.virtualbox.org/virtualbox/debian $(lsb_release -cs) contrib non-free" |sudo tee /etc/apt/sources.list.d/virtualbox.list
+    wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
+    sudo apt update
+    sudo apt install -y virtualbox virtualbox-guest-additions-iso
 
-sudo apt install -y docker rxvt-unicode-256color
+    sudo apt install -y docker rxvt-unicode-256color
 
 
-sudo usermod -a -G docker volker
+    sudo usermod -a -G docker volker
 
-sudo apt install -y mplayer vlc
-sudo apt install -y openssh-server
-sudo apt install -y duplicity python-boto
+    sudo apt install -y mplayer
+    sudo apt install -y openssh-server
+    sudo apt install -y duplicity python-boto
 
-cd /tmp
-wget https://releases.hashicorp.com/vagrant/1.8.7/vagrant_1.8.7_x86_64.deb
-sudo dpkg -i vagrant_1.8.7_x86_64.deb
+    cd /tmp
+    wget https://releases.hashicorp.com/vagrant/1.8.7/vagrant_1.8.7_x86_64.deb
+    sudo dpkg -i vagrant_1.8.7_x86_64.deb
 
-sudo apt-get remove -y --purge firefox
+    sudo apt-get remove -y --purge firefox
+
+    sudo apt autoremove
+}
+
+macos() {
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+}
+
+usage() {
+    echo "$0 <argument>:"
+    echo "   $0 ubuntu"
+    echo "   $0 macos"
+}
+
+main() {
+    if [ -z "$1" ]; then
+       usage
+       exit 1
+    fi
+
+    case "$1" in
+        ubuntu)
+            ubuntu
+            ;;
+        "macos")
+            macos
+            ;;
+        *)
+            usage
+            ;;
+    esac
+}
+
+main $@
