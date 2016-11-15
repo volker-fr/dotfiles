4acfa92c9898e834ecce9a66cb9f0335ce8fc5de
Volker <lists.volker@gmail.com>
2016-11-15 11:41:06 -0500

    automate package installation

--- install.sh
+++ install.sh
@@ -12,17 +12,19 @@ sudo apt install -y i3
 echo "deb http://download.virtualbox.org/virtualbox/debian $(lsb_release -cs) contrib non-free" |sudo tee /etc/apt/sources.list.d/virtualbox.list
 wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
 sudo apt update
-sudo apt install virtualbox virtualbox-guest-additions-iso
+sudo apt install -y virtualbox virtualbox-guest-additions-iso
 
-sudo apt install docker
+sudo apt install -y docker rxvt-unicode-256color
 
 
 sudo usermod -a -G docker volker
 
-sudo apt install mplayer vlc
-sudo apt install openssh-server
-sudo apt install duplicity python-boto
+sudo apt install -y mplayer vlc
+sudo apt install -y openssh-server
+sudo apt install -y duplicity python-boto
 
 cd /tmp
 wget https://releases.hashicorp.com/vagrant/1.8.7/vagrant_1.8.7_x86_64.deb
 sudo dpkg -i vagrant_1.8.7_x86_64.deb
+
+sudo apt-get remove -y --purge firefox
