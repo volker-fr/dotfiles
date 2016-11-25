5a3ac29daaa55a0f09bdcabaf36d7b0208c4bb8b
Volker <lists.volker@gmail.com>
2016-11-24 22:12:25 -0500

    Add packages into own function & disable sleep on laptop lid close

--- install.sh
+++ install.sh
@@ -1,5 +1,13 @@
 #!/bin/sh -e
-ubuntu() {
+
+disableLidCloseSleep() {
+    if ! grep -q "^HandleLidSwitch" /etc/systemd/logind.conf; then
+        echo "HandleLidSwitch=ignore" |sudo tee -a /etc/systemd/logind.conf > /dev/null
+        sudo service systemd-logind restart
+    fi
+}
+
+ubuntuPackages() {
     sudo apt install -y encfs
     sudo apt install -y iotop vim git redshift-gtk tmux keepass2
     sudo apt install -y owncloud-client
@@ -14,25 +22,32 @@ ubuntu() {
     echo "deb http://download.virtualbox.org/virtualbox/debian $(lsb_release -cs) contrib non-free" |sudo tee /etc/apt/sources.list.d/virtualbox.list
     wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
     sudo apt update
-    sudo apt install -y virtualbox virtualbox-guest-additions-iso
+    sudo apt install -y virtualbox-5.1
 
     sudo apt install -y docker rxvt-unicode-256color
-
-
-    sudo usermod -a -G docker volker
 
     sudo apt install -y mplayer
     sudo apt install -y openssh-server
     sudo apt install -y duplicity python-boto
     sudo apt install -y libsource-highlight-common
 
-    cd /tmp
-    wget https://releases.hashicorp.com/vagrant/1.8.7/vagrant_1.8.7_x86_64.deb
-    sudo dpkg -i vagrant_1.8.7_x86_64.deb
+    vagrantVersion=1.8.7
+    if ! dpkg -l|grep vagrant|grep -q "$vagrantVersion"; then
+        cd /tmp
+        wget https://releases.hashicorp.com/vagrant/${vagrantVersion}/vagrant_${vagrantVersion}_x86_64.deb
+        sudo dpkg -i vagrant_${vagrantVersion}_x86_64.deb
+    fi
 
     sudo apt-get remove -y --purge firefox
 
-    sudo apt autoremove
+    sudo apt autoremove -y
+}
+
+ubuntu() {
+    ubuntuPackages
+    disableLidCloseSleep
+
+    sudo usermod -a -G docker volker
 }
 
 macos() {
