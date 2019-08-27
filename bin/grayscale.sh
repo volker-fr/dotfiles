bb56667554d114c1ad55add51d970b58d2db4983
Volker <lists.volker@gmail.com>
2019-08-26 23:11:47 -0500

    Add script to enable grayscale

---
+++ bin/grayscale.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+compton --backend glx --glx-fshader-win \
+"
+uniform float opacity;
+uniform bool invert_color;
+uniform sampler2D tex;
+
+void main() {
+    vec4 c = texture2D(tex, gl_TexCoord[0].xy);
+    float y = dot(c.rgb, vec3(0.299, 0.587, 0.114));
+    gl_FragColor = vec4(y, y, y, 1.0);
+}
+"
